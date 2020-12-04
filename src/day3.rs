9c0c65dbfe40a53f818626e755ec5dac7abc89b8
Brandon Edens <brandonedens@gmail.com>
2020-12-03 20:13:21 -0800

    Check in day 3 solution.

---
+++ src/day3.rs
@@ -0,0 +1,48 @@
+use anyhow::Result;
+use std::fs;
+
+fn count_num_trees(map: &str, slope: (usize, usize)) -> usize {
+    let lines: Vec<&str> = map.lines().collect();
+    let height = lines.len();
+    let width = lines.iter().next().unwrap().chars().count();
+
+    let mut num_trees = 0;
+    let mut x = 0;
+    let mut y = 0;
+    while y < height {
+        if lines[y].chars().nth(x % width).unwrap() == '#' {
+            num_trees += 1;
+        }
+        y += slope.1;
+        x += slope.0;
+    }
+    num_trees
+}
+
+pub fn day3() -> Result<()> {
+    /*
+        let map = r#"..##.......
+    #...#...#..
+    .#....#..#.
+    ..#.#...#.#
+    .#...##..#.
+    ..#.##.....
+    .#.#.#....#
+    .#........#
+    #.##...#...
+    #...##....#
+    .#..#...#.#"#;
+        */
+    let map = include_str!("day3_input.txt");
+    let num_trees = count_num_trees(&map, (3, 1));
+    println!("num_trees: {}", num_trees);
+
+    let slopes = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let val = slopes
+        .iter()
+        .map(|slope| count_num_trees(&map, *slope))
+        .fold(1, |acc, x| acc * x);
+    println!("multi all values: {}", val);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod day1;
 mod day2;
+mod day3;
 
 use anyhow::Result;
 use day1::*;
 use day2::*;
+use day3::*;
 
 fn main() -> Result<()> {
-    day2()?;
+    day3()?;
 
     Ok(())
 }
